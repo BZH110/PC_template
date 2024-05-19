19423e2768801f8ae788c4985d9c40aedff8811c
baoziheng <baoziheng@kuaishou.com>
2024-05-19 11:34:39 +0800

    fix

--- templates/SegmentTree.h
+++ templates/SegmentTree.h
@@ -216,7 +216,7 @@ using ll = long long;
                 a.push_back(e);
                 n++;
             }
-            dat.resize(4 * n + 5);
+            dat.resize(4 * n + 5, e);
             init(0, 0, n - 1, a);
         }
 
